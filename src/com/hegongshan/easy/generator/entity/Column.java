fb9e6533dd897026961ec31e3f0da820fa38c4e0
hegongshan <hegongshan@shan>
2018-03-24 15:40:48 +0800

    override equals,hashCode,and compareTo

--- src/com/hegongshan/easy/generator/entity/Column.java
+++ src/com/hegongshan/easy/generator/entity/Column.java
@@ -1,6 +1,6 @@
 package com.hegongshan.easy.generator.entity;
 
-public class Column {
+public class Column implements Comparable<Column> {
 	private String columnName;
 	private String columnType;
 
@@ -20,4 +20,40 @@ public class Column {
 		this.columnType = columnType;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((columnName == null) ? 0 : columnName.hashCode());
+		result = prime * result + ((columnType == null) ? 0 : columnType.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Column other = (Column) obj;
+		if (columnName == null) {
+			if (other.columnName != null)
+				return false;
+		} else if (!columnName.equals(other.columnName))
+			return false;
+		if (columnType == null) {
+			if (other.columnType != null)
+				return false;
+		} else if (!columnType.equals(other.columnType))
+			return false;
+		return true;
+	}
+
+	@Override
+	public int compareTo(Column other) {
+		return columnName.compareTo(other.getColumnName());
+	}
+	
 }
